b285bd67aa9d7bc72afad9476c3c68babae41bf9
MilaniTetani <milanitetani@gmail.com>
2025-04-10 23:13:33 +0200

    Completed Task1 method

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -7,6 +7,17 @@ public class SimpleFunctions {
 
     // Task 1: by 22T4905
     public int Task1 (List<Integer> a, List<Integer> b) {
-        return 0;
+        // list a is a list of integers
+        // list b is a list of indices
+
+        int sum = 0;
+        for (int i : b) // for each element in b
+        {
+           if (i >= 0 && i < a.size())
+           {
+               sum += a.get(i); // add the element(s) at index i in list a to sum
+           }
+        }
+        return sum; // returns the sum of the elements in a at the indices specified in b
     }
 }
