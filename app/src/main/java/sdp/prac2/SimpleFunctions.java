be9e2b7e2ecf635e91572899b38875cee0ba9e89
MilaniTetani <milanitetani@gmail.com>
2025-04-10 22:24:31 +0200

    Initialized Task1 method

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,9 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    // Task 1: by 22T4905
+    public int Task1 (List<Integer> a, List<Integer> b) {
+        return 0;
+    }
 }
